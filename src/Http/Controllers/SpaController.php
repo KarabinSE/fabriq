0aa14957abac8ec634a5a3807a0a15751cfe7fa2
Albin N <albin@jnilsson.id>
2026-04-14 07:54:29 +0200

    re-add view string return

--- src/Http/Controllers/SpaController.php
+++ src/Http/Controllers/SpaController.php
@@ -2,17 +2,21 @@
 
 namespace Karabin\Fabriq\Http\Controllers;
 
+use Illuminate\Contracts\View\View;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class SpaController extends Controller
 {
-    public function index(Request $request): JsonResponse
+    public function index(Request $request): JsonResponse|View
     {
         if ($request->wantsJson()) {
             return response()->json('Get outta here!', 404);
         }
 
-        abort(404);
+        /** @var view-string $viewString * */
+        $viewString = 'vendor.fabriq.index';
+
+        return view($viewString);
     }
 }
