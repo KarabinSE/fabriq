41d4bb1754d710bc15fe332723a0ea40522870f0
Albin N <albin@jnilsson.id>
2026-06-03 14:56:32 +0200

    update packages, use vite 5

---
+++ vite.config.mjs
@@ -0,0 +1,22 @@
+import vue from '@vitejs/plugin-vue2'
+import laravel from 'laravel-vite-plugin'
+import { defineConfig, splitVendorChunkPlugin } from 'vite'
+
+export default defineConfig({
+    plugins: [
+        laravel([
+            // 'resources/css/app.css',
+            'resources/js/fabriq.js',
+        ]),
+
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        }),
+        splitVendorChunkPlugin()
+    ],
+})
