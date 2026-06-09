02ff3b579990d5111091bc9108e2e09dccab32b4
cleanSumo <johannes@karabin.se>
2026-06-09 09:38:34 +0200

    update eslint lint vue plugin

--- resources/js/smart-blocks/Edit.vue
+++ resources/js/smart-blocks/Edit.vue
@@ -149,7 +149,7 @@
                                             <KeepAlive>
                                                 <Component
                                                     :is="block.block_type.component_name"
-                                                    v-model:content="localizedContent[activeLocale].boxes[boxIndex]"
+                                                    v-model="localizedContent[activeLocale].boxes[boxIndex]"
                                                     :value="block"
                                                     :index="boxIndex"
                                                     @input="block = $event.target.value"
